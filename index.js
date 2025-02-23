95554f1b2fb249fb4224cc6bd1eceee0efd94385
Mueslint <solom.emmanuel@gmail.com>
2025-02-23 20:20:23 +0100

    feat(Factory): remove Factory to keep the original Drug instanciation to avoid possible breaking changes

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-import fs from "fs";
+import fs from 'fs';
 
-import { DrugFactory } from "./src/factories/DrugFactory";
-import { Pharmacy } from "./src/models/pharmacy";
+import { Drug } from './src/models/Drug';
+import { Pharmacy } from './src/models/Pharmacy';
 
-const log = [];
 const drugs = [
-  DrugFactory.createDrug("Doliprane", 20, 30),
-  DrugFactory.createDrug("Herbal Tea", 10, 5),
-  DrugFactory.createDrug("Fervex", 12, 35),
-  DrugFactory.createDrug("Magic Pill", 15, 40),
-  DrugFactory.createDrug("Dafalgan", 20, 30),
+  new Drug('Doliprane', 20, 30),
+  new Drug('Herbal Tea', 10, 5),
+  new Drug('Fervex', 12, 35),
+  new Drug('Magic Pill', 15, 40),
+  new Drug('Dafalgan', 20, 30),
 ];
 const pharmacy = new Pharmacy(drugs);
+
+const log = [];
 
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
   log.push(JSON.parse(JSON.stringify(pharmacy.updateBenefitValue())));
 }
 
+/* eslint-disable no-console */
 fs.writeFile(
-  "output.json",
-  JSON.stringify({ result: log }, null, 2).concat("\n"),
+  'output.json',
+  JSON.stringify({ result: log }, null, 2).concat('\n'),
   (err) => {
     if (err) {
-      console.log("error");
+      console.log('error');
     } else {
-      console.log("success");
+      console.log('success');
     }
-  },
+  }
 );
+
+/* eslint-enable no-console */
--- src/models/Drug.js
+++ src/models/Drug.js
@@ -1,16 +1,28 @@
-import { DefaultStrategy } from "../strategies/DefaultStrategy";
+import {
+  DefaultStrategy,
+  HerbalTeaStrategy,
+  MagicPillStrategy,
+  FervexStrategy,
+  DafalganStrategy,
+} from '../strategies';
 
+const strategiesMap = {
+  'Herbal Tea': HerbalTeaStrategy,
+  Fervex: FervexStrategy,
+  'Magic Pill': MagicPillStrategy,
+  Dafalgan: DafalganStrategy,
+};
 export class Drug {
-  constructor(
-    name,
-    expiresIn,
-    benefit,
-    updateStrategy = new DefaultStrategy(),
-  ) {
+  constructor(name, expiresIn, benefit) {
     this.name = name;
     this.expiresIn = expiresIn;
     this.benefit = benefit;
-    this.updateStrategy = updateStrategy;
+    this.updateStrategy = this.getStrategy(name);
+  }
+
+  getStrategy(name) {
+    const UpdateStrategy = strategiesMap[name] || DefaultStrategy;
+    return new UpdateStrategy();
   }
 
   update() {
